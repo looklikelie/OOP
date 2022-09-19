b78d8332e58487c3271c5dae76db9f02de4c2bc6
looklikelie <quilimonjaro@gmail.com>
2022-09-19 14:20:19 +0300

    feat: 1st lab with tests

---
+++ Isu/Models/CourseNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Isu.Tools;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Isu.Models
+{
+    public class CourseNumber
+    {
+        public CourseNumber(string name)
+        {
+            if (name[2] < '1' || name[2] > '4') throw new IsuException("Invalid group name");
+            if (name[0] != 'M') throw new IsuException("Invalid group name");
+            if (name[1] != '3') throw new IsuException("Invalid group name");
+            Coursenum = name[2];
+        }
+
+        protected CourseNumber()
+        {
+        }
+
+        public int Coursenum { get; protected set; }
+    }
+}
